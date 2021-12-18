b85583d746c55c9b07d1c989d0d52db727d04ce5
terilondon <teri.london@gmail.com>
2021-12-18 00:17:41 -0800

    well that definitely did not work even when i fixed the very obvious thing that i missed but im having a lot of fun

--- seed.js
+++ seed.js
@@ -4,8 +4,8 @@
 const db = require("./models");
 
 const newArticle = {
-	title: { type: String, required: true },
-	body: { type: String, required: true },
+	title: "the sad story of the seed.js file",
+	body: "if we water this file, will it grow? what shall nourish our young data? what do you need, seed?",
 };
 
 db.Article.create(newArticle, (err, article) => {
